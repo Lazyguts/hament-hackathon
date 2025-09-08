08c97937c2df2b45ef78c5f235f1b495566c9ad7
Lazyguts <hemantkushwaka24102004@gmail.com>
2025-09-08 18:53:37 +0530

    commit befor adding ai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Consultants from "./Consultants";
-import FindLawyer from "./FindLawyer";
-import Navbar from "./Navbar";
+import ChatBox from "./home/Chat";
+import Consultants from "./home/Consultants";
+import FindLawyer from "./home/FindLawyer";
+import Navbar from "./home/Navbar";
 
 export default function Home() {
   return (
@@ -10,26 +11,7 @@ export default function Home() {
        <Navbar/>
 
         <main className="flex flex-col gap-8">
-          <section className="bg-white rounded-lg p-6 shadow-md border border-gray-200">
-            <h2 className="align-center text-2xl font-bold mb-4">Nay.ai</h2>
-            <div className="relative mb-4">
-              <input
-                type="text"
-                placeholder="Ask anything to Nay.ai"
-                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500"
-              />
-            </div>
-            <div className="flex justify-between items-start">
-              <div className="flex items-center justify-center w-32 h-10 rounded-2xl font-semibold text-black bg-white/10 backdrop-blur-md border border-white/30 shadow-lg hover:bg-black/20 hover:shadow-xl transition-all duration-300">
-                <h3 className="font-semibold text-black-500">History</h3>
-              </div>
-              <div className="flex flex-col items-center ml-4">
-                <span className="flex items-center justify-center w-32 h-10 rounded-2xl font-semibold text-black bg-white/10 backdrop-blur-md border border-white/30 shadow-lg hover:bg-black/20 hover:shadow-xl transition-all duration-300">
-                  Document
-                </span>
-              </div>
-            </div>
-          </section>
+          <ChatBox/>
 
           <Consultants/>
 
